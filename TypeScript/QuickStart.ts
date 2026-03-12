42a265defc9820e66f36894d2e6e00bf860d960e
BoilTask <BoilTask@qq.com>
2026-03-12 11:35:49 +0800

    更新自动mixin插件

--- TypeScript/QuickStart.ts
+++ TypeScript/QuickStart.ts
@@ -1,7 +1,7 @@
 import * as UE from "ue";
 import { argv, toDelegate } from "puerts";
 
-import { ToMinix } from "./AutoMinix";
+import { ToMinix } from "./AutoMixin";
 import "./System/Log/index"; // 导入日志系统，自动扩展console功能
 
 let gameInstance = argv.getByName("GameInstance") as UE.TemplateGameInstance;
