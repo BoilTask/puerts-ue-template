d7b642e8486c9f7e8f56e65796b02b902364a4b8
BoilTask <BoilTask@qq.com>
2026-03-23 18:27:04 +0800

    增加js生成

--- Source/Template.Target.cs
+++ Source/Template.Target.cs
@@ -11,5 +11,10 @@ public class TemplateTarget : TargetRules
 		DefaultBuildSettings = BuildSettingsVersion.V6;
 		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_7;
 		ExtraModuleNames.Add("Template");
+
+		// 每次编译在项目根目录运行npm run build
+		string projectRoot = ProjectFile.Directory.FullName;
+		string tsCmd = $"cd /D \"{projectRoot}\" && npm run build";
+		PostBuildSteps.Add(tsCmd);
 	}
 }
